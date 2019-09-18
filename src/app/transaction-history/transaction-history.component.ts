f2d8993d8239fdb9b4425f7e3ea21eab4b396aeb
Sergey <asd@mail.ru>
2019-09-18 14:35:56 +0300

    Add responsiveness using Bootstrap

--- src/app/transaction-history/transaction-history.component.ts
+++ src/app/transaction-history/transaction-history.component.ts
@@ -10,7 +10,10 @@ import { TransactionService } from '../transaction.service';
 export class TransactionHistoryComponent implements OnInit {
   private transactions: Transaction[];
   private selectedTransaction: Transaction;
+  private deletedTransaction: Transaction;
 
+
+  isDeleted = false;
   balance: number;
 
   constructor(private transactionService: TransactionService) {
@@ -25,11 +28,16 @@ export class TransactionHistoryComponent implements OnInit {
     this.selectedTransaction = transaction;
   }
 
+  isSelectedToDelete(transaction: Transaction) {
+    return transaction === this.deletedTransaction;
+  }
+
   isSelected(transaction: Transaction) {
     return transaction === this.selectedTransaction;
   }
 
   deleteTransaction(transaction: Transaction) {
+    this.deletedTransaction = transaction;
     this.transactionService.deleteTransaction(transaction).subscribe(() => {
       this.transactions = this.transactions.filter(t => t !== transaction);
     });
